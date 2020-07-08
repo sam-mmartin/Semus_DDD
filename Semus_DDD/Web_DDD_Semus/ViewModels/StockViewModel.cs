538e0ef3c42750d59bed06b053da14ed0bbbaaf7
sam-mmartin <smeirelesmartins@gmail.com>
2020-07-08 11:41:25 -0300

    Atualizado view createStock

--- Semus_DDD/Web_DDD_Semus/ViewModels/StockViewModel.cs
+++ Semus_DDD/Web_DDD_Semus/ViewModels/StockViewModel.cs
@@ -5,6 +5,7 @@ namespace Web_DDD_Semus.ViewModels
     public class StockViewModel
     {
         [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo Descrição é obrigatório")]
         public string Description { get; set; }
     }
 }
